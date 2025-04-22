33d8ac841332c7a564d0fb22c1a9b5b8908bc71d
Isaac Rojas Sosa <isaacrojassosa@gmail.com>
2025-04-21 19:22:53 -0600

    matching

--- MtdrSpring/backend/src/main/frontend/src/App.js
+++ MtdrSpring/backend/src/main/frontend/src/App.js
@@ -6,7 +6,7 @@ import ManageTasks from './ManageTasks';
 import Projects from './pages/Projects';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+ 
 function App() {
   const [page, setPage] = useState('Home');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
