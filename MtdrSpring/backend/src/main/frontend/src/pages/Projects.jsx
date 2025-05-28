603cd9274baee04b8d52b4b75b013d27c1f4872a
santiagosauma <113849899+santiagosauma@users.noreply.github.com>
2025-05-27 23:12:21 -0600

    Fixed Tag

--- MtdrSpring/backend/src/main/frontend/src/pages/Projects.jsx
+++ MtdrSpring/backend/src/main/frontend/src/pages/Projects.jsx
@@ -5,7 +5,7 @@ function Projects({ onSelectProject }) {
 return (
   <div className="w-full min-h-screen flex flex-col bg-gray-50">
     <header className="bg-[#423E3A] h-12 sm:h-16 md:h-[65px] px-3 sm:px-5 flex items-center shadow-md mb-1 md:mb-2">
-      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">Error!!!</h1>
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">All Projects</h1>
     </header>
     <div className="flex-1 min-h-0 p-2 sm:p-4">
       <ProjectsTable onSelectProject={onSelectProject} />
